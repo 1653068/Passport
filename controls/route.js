e8c8c99a9f2fa0a90cb324c93e1c2bd260b65923
1653068 <1653068@student.hcmus.edu.vn>
2019-04-10 22:45:36 +0700

    fix

--- controls/route.js
+++ controls/route.js
@@ -65,7 +65,7 @@ module.exports = function (app, passport) {
     });
 
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect: '/', // redirect to the secure profile section
+        successRedirect: '/profile', // redirect to the secure profile section
         failureRedirect: '/signup', // redirect back to the signup page if there is an error
         failureFlash: true // allow flash messages
     }));
@@ -78,7 +78,7 @@ module.exports = function (app, passport) {
 
     app.get('/auth/facebook/callback',
         passport.authenticate('facebook', {
-            successRedirect: '/profile',
+            successRedirect: '/',
             failureRedirect: '/login',
             failureFlash: true
         }));
