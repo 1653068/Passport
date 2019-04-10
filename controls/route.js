da9987fa975102ceb01c1cf6a3de539621323459
1653068 <1653068@student.hcmus.edu.vn>
2019-04-10 23:33:30 +0700

    profile

--- controls/route.js
+++ controls/route.js
@@ -78,12 +78,10 @@ module.exports = function (app, passport) {
 
     app.get('/auth/facebook/callback',
         passport.authenticate('facebook', {
-            failureRedirect: '/login'
-        }),
-        function (req, res) {
-            // Successful authentication, redirect home.
-            res.redirect('/');
-        });
+            successRedirect: '/',
+            failureRedirect: '/login',
+            failureFlash: true
+        }));
 
 
     // =====================================
