137bfd206782084a0eb8c8f953452ebea16e9e0f
1653068 <1653068@student.hcmus.edu.vn>
2019-04-10 22:23:44 +0700

    facebook fix

--- controls/route.js
+++ controls/route.js
@@ -76,7 +76,7 @@ module.exports = function (app, passport) {
     app.get('/auth/facebook',
         passport.authenticate('facebook'));
 
-    app.get('/auth/facebook/callback',
+    app.post('/auth/facebook/callback',
         passport.authenticate('facebook', {
             successRedirect: '/profileFacebook',
             failureRedirect: '/login',
